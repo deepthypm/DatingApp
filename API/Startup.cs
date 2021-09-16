53d3ac775c7df362c822ec06094eadca871d25d3
deepthypm <deepthypm@gmail.com>
2021-09-16 20:37:28 +0530

    End of section 4

--- API/Startup.cs
+++ API/Startup.cs
@@ -13,6 +13,12 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using API.Data;
+using API.Services;
+using API.Interfaces;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using API.Extensions;
 
 namespace API
 {
@@ -21,24 +27,21 @@ namespace API
         private readonly IConfiguration _config;
         public Startup(IConfiguration config)
         {
-           _config = config;
+            _config = config;
         }
 
-       
-       // public IConfiguration Configuration { get; }
+
+        // public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<DataContext>(options =>
-            {
-                options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
-            }
-            );
+            services.AddApplicationServices(_config);
             services.AddControllers();
             services.AddCors();
+            services.AddIdentityServices(_config);
             //services.AddSwaggerGen(c =>
-           // {
+            // {
             //    c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
             //});
         }
@@ -57,8 +60,9 @@ namespace API
 
             app.UseRouting();
 
-            app.UseCors(x=>x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
